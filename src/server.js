40e68064f6a658dde7a8679cf23e273d575003c0
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-07-14 00:01:43 +0900

    logged in user

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
-
 import express from "express";
+import session from "express-session";
 import morgan from "morgan";
 
+import { localsMiddleware } from "./middlewares";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
-
 
 const app = express();
 const logger = morgan("dev");
@@ -15,6 +14,16 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "hello",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
